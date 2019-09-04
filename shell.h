e2f1561944b3488235b3596c9122a29d0f9fa3a1
Flrotm <39736679+Flrotm@users.noreply.github.com>
2019-09-04 16:32:24 -0500

    Update shell.h

--- shell.h
+++ shell.h
@@ -2,13 +2,27 @@
 #define SHELL_H
 
 #include "sort.h"
-
+#include <iostream>
 class ShellSort : public Sort {       
     public:
         ShellSort(int *elements, size_t size) : Sort(elements, size) {}
-
+    void swap(int *a, int *b)
+    {
+        int temp = *a;
+        *b = *a;
+        *a = temp;
+    }
         void execute() {
-            // TODO
+            int temp;
+            for(int i=size/2;i>0;i/=2){
+                for(int j=i;j<size;j++){
+                    for(int h=j-i;h >=0;h-=i){
+                        if(elements[h+1]>=elements[h])break; else{
+                            swap(&elements[h],&elements[h+1]);
+                        }
+                    }
+                }
+            }
         }
 
         inline string name() { return "ShellSort"; }
