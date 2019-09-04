9de6223a1741744ff2e8e2f3ec11f8ad5813b9c5
Flrotm <39736679+Flrotm@users.noreply.github.com>
2019-09-04 16:59:18 -0500

    Update quick.h

--- quick.h
+++ quick.h
@@ -3,15 +3,40 @@
 
 #include "sort.h"
 
-class QuickSort : public Sort {       
-    public:
-        QuickSort(int *elements, size_t size) : Sort(elements, size) {}
+class QuickSort : public Sort {
+public:
+    QuickSort(int *elements, size_t size) : Sort(elements, size) {}
 
-        void execute() {
-            // TODO
+    void execute(){
+        quickSort(0,size-1);
+    }
+    void quickSort(int low, int high){
+        if(low < high){
+            int i=partition(low, high);
+            quickSort(low,i-1);
+            quickSort(i+1,high);
         }
+    }
 
-        inline string name() { return "QuickSort"; }
+    int partition(int low, int high){
+        int pivot = elements[low];
+        int i=low;
+        int j=high;
+
+        while(i<j){
+            while(pivot<elements[j]){
+                j--;}
+            while(i<j and elements[i]<=pivot){
+                i++;}
+            if(i<j){
+                swap(elements[i],elements[j]);}
+        }
+        swap(elements[j],elements[low]);
+        return (i);
+
+    }
+
+    inline string name() { return "QuickSort"; }
 };
 
 #endif
