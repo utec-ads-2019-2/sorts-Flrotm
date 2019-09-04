e9345e73d112184259d45e48c551ad5ad24c2066
Flrotm <39736679+Flrotm@users.noreply.github.com>
2019-09-04 16:24:04 -0500

    Update bubble.h

--- bubble.h
+++ bubble.h
@@ -3,13 +3,27 @@
 
 #include "sort.h"
 #include <algorithm>
+#include <vector>
+#include <iostream>
 
 class BubbleSort : public Sort {       
     public:
         BubbleSort(int *elements, size_t size) : Sort(elements, size) {}
+    void swap(int *a, int *b)
+    {
+            int temp = *a;
+            *b = *a;
+            *a = temp;
+    }
+    void execute() {
+            int i, j;
+            for (i = 0; i<size; i++){
+                for (j = 0; j < size; j++){ 
+                    if (elements[j] > elements[j+1])
+                        swap(&elements[j], &elements[j+1]);}}
 
-        void execute() {
-            // TODO
+
+
         }
 
         inline string name() { return "BubbleSort"; }
