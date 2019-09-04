84de92bce5dbd04dd3a0d683b952a8b5fa8a4a3e
Flrotm <39736679+Flrotm@users.noreply.github.com>
2019-09-04 16:31:42 -0500

    Update insert.h

--- insert.h
+++ insert.h
@@ -2,13 +2,25 @@
 #define INSERT_H
 
 #include "sort.h"
+#include <iostream>
 
 class InsertSort : public Sort {       
     public:
         InsertSort(int *elements, size_t size) : Sort(elements, size) {}
 
         void execute() {
-            // TODO
+            int j,k=0;
+            for(int i=0;i<size;i++){
+                k=elements[i];
+                j=i-1;
+
+                while(j >= 0 and elements[j]>k){
+                    elements[j+1]=elements[j];
+                    j--;
+                }
+                elements[j+1]=k;
+
+            }
         }
 
         inline string name() { return "InsertSort"; }
