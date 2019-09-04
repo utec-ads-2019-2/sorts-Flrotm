a69409ecc055dd5133f293d5428431461cbe706b
Flrotm <39736679+Flrotm@users.noreply.github.com>
2019-09-04 17:00:22 -0500

    Update select.h

--- select.h
+++ select.h
@@ -2,13 +2,26 @@
 #define SELECT_H
 
 #include "sort.h"
+#include <iostream>
 
 class SelectSort : public Sort {       
     public:
         SelectSort(int *elements, size_t size) : Sort(elements, size) {}
-
+    void swap(int *a, int *b)
+    {
+        int temp = *a;
+        *b = *a;
+        *a = temp;
+    }
         void execute() {
-            // TODO
+            int min=0;
+            for(int i=0;i<size;i++){
+                min=i;
+            for(int j=i+1;j<size;j++){
+              if(elements[j]<elements[min]) {
+                  min = j; }
+              swap(&elements[min],&elements[i]); }
+            }
         }
 
         inline string name() { return "SelectSort"; }
